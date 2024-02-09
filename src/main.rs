5a71c64ab8575eee06d4f8016daca0756f98f863
David Feldner <dafe@itu.dk>
2024-02-09 12:59:08 +0100

    init actix cargo project

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_files as fs;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(fs::Files::new("/static", "../static").index_file("index.html"))
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
