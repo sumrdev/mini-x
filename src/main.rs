464ab9107cc3271f061146e79532a7b970537e9a
David Feldner <dafe@itu.dk>
2024-02-12 11:02:17 +0100

    Working simple template
    Co-authored-by: Marius Nielsen <sumrdev@users.noreply.github.com>
    Co-authored-by: AlbinoLoaf <AlbinoLoaf@users.noreply.github.com>
    Co-authored-by: Daniel-127 <Daniel-127@users.noreply.github.com>
    Co-authored-by: Markusgp <Markusgp@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{ get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{ get, App, HttpServer, Responder};
 use actix_files as fs;
 use askama_actix::{Template};
+use chrono::{DateTime,Utc};
+use rusqlite::{ Connection, Result};
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "../templates/hello.html")] // using the template in this path, relative
@@ -9,56 +11,150 @@ struct HelloTemplate<'a> { // the name of the struct can be anything
                    // in your template
 }
 struct User {
-    user_id: String,
+    user_id: i64,
     user_name: String,
 } 
+struct Messages {
+    text: String,
+    email:String,
+    username: String,
+    pub_date: DateTime<Utc>,
+}
 // https://doc.rust-lang.org/std/vec/index.html
 // https://doc.rust-lang.org/std/option/enum.Option.html
 #[derive(Template)] // this will generate the code...
-#[template(path = "../templates/layout.html")] // using the template in this path, relative
-struct LayoutTemplate<'a> {// should be used as a wrapper not sure how
+#[template(path = "../templates/simpleTemplate.html")] // using the template in this path, relative
+struct SimpleTemplate {// should be used as a wrapper not sure how
+    messages: Vec<Messages> //Option with messages aka options(vec) or just a vec
+}
+
+struct LayoutTemplateOld<'a> {// should be used as a wrapper not sure how
     title: &'a str,
     body: &'a str,
-    g_user: Option<User>,// Optione is a nullable field user not defined
-    flashes: None//Option with messeges aka options(vec) or just a vec
+    g: Option<G>,// Option is a nullable field user not defined
+    flashes: Option<Vec<Messages>> //Option with messages aka options(vec) or just a vec
 }
-#[derive(Template)]
-#[template(path = "../templates/timeline.html")] 
+// String
+struct G {
+    db: Connection,
+    user: User,
+}
+//#[derive(Template)]
+//#[template(path = "../templates/timeline.html")] 
 struct TimelineTemplate<'a> {
     name: String, // Is it not title 
-    messages:Vec<String>,// Vec<Message>, dynamic array of mesege structs 
+    messages:Vec<Messages>,// Vec<Message>, dynamic array of message structs 
     user: Option<User>,
-    request_endpoint: &'a str,//just an URL does not need to be strct
+    request_endpoint: &'a str,//just an URL does not need to be strict
     profile_user: Option<User>,
-    followed: bool,//Unsure how to difine this properly
+    followed: bool,//Unsure how to define this properly
 }
-
-
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(timeline)
             .service(fs::Files::new("/static", "../static").index_file("index.html"))
-            .route("/hey", web::get().to(manual_hello))
-            .route("/test", web::get().to(render_hello_template))
+            
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
+fn get_database_string() -> String {
+    "/tmp/mini-x.db".to_string()
+}
+
+fn connect_db() ->  Result<Connection>  {
+    Connection::open(get_database_string())
+}
+
+fn init_db()  -> rusqlite::Result<()>{
+    let schema_sql = std::fs::read_to_string("schema.sql").unwrap();
+    let conn = connect_db().unwrap();
+
+    conn.execute_batch(&schema_sql)?;
+    Ok(())
+}
+
+fn query_db(query: &str) {
+    
+}
+
+fn get_user_id(username: &str ) {
+
+}
+
+fn g_mock() -> Result<G, rusqlite::Error> {
+    let connection = Connection::open_in_memory()?;
+    Ok(G {
+        db: connection,
+        user: User {
+            user_id: 1,
+            user_name: String::from("Test Name"),
+        },
+    })
+}
+
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn timeline() -> impl Responder {
+    let messages: Vec<Messages> = vec![
+        Messages {
+            text: String::from("Hello, world!"),
+            email: String::from("example@email.com"),
+            username: String::from("user123"),
+            pub_date: Utc::now(),
+        },
+        Messages {
+            text: String::from("How are you?"),
+            email: String::from("another@email.com"),
+            username: String::from("user456"),
+            pub_date: Utc::now(),
+        },
+    ];
+
+    return SimpleTemplate { messages };
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+#[get("/public")]
+async fn public_timeline() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
 }
 
-async fn render_hello_template() -> impl Responder {
+#[get("/{username}")]
+async fn user_timeline() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
+}
+
+#[get("/{username}/follow")]
+async fn follow_user() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
+}
+
+#[get("/{username}/unfollow")]
+async fn unfollow_user() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
+}
+
+#[get("/add_message")]
+async fn add_message() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
+}
+
+#[get("/login")]
+async fn login() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
+}
+
+#[get("/register")]
+async fn register() -> impl Responder {
+    return HelloTemplate { name: "AAAA" };
+}
+
+#[get("/logout")]
+async fn logout() -> impl Responder {
     return HelloTemplate { name: "AAAA" };
 }
