bc995983396e8d523068e076b72fbbb379bb44da
David Feldner <dafe@itu.dk>
2024-02-12 11:53:11 +0100

    Extended working simple template
    Co-authored-by: Daniel <midf@itu.dk>
    Co-authored-by: David  <dafe@itu.dk>
    Co-authored-by: Marius <mawn@itu.dk>
    Co-authored-by: Marius <mariu@itu.dk>
    Co-authored-by: Markus <mgrp@itu.dk>

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,29 @@
-use actix_web::{ get, App, HttpServer, Responder};
 use actix_files as fs;
-use askama_actix::{Template};
-use chrono::{DateTime,Utc};
-use rusqlite::{ Connection, Result};
+use actix_web::{post, get, App, HttpServer,HttpResponse, Responder, cookie::Key};
+use actix_session::{Session, SessionMiddleware, storage::CookieSessionStore};
+use askama_actix::Template;
+use chrono::{DateTime, Utc};
+use rusqlite::{Connection, Result};
+use uuid::Uuid;
 
 #[derive(Template)] // this will generate the code...
 #[template(path = "../templates/hello.html")] // using the template in this path, relative
-struct HelloTemplate<'a> { // the name of the struct can be anything
+struct HelloTemplate<'a> {
+    // the name of the struct can be anything
     name: &'a str, // the field name should match the variable name
                    // in your template
 }
 struct User {
-    user_id: i64,
-    user_name: String,
-} 
+    user_id: Uuid,
+    username: String,
+}
+struct G {
+    db: Connection,
+    user: User,
+}
 struct Messages {
     text: String,
-    email:String,
+    email: String,
     username: String,
     pub_date: DateTime<Utc>,
 }
@@ -24,49 +31,50 @@ struct Messages {
 // https://doc.rust-lang.org/std/option/enum.Option.html
 #[derive(Template)] // this will generate the code...
 #[template(path = "../templates/simpleTemplate.html")] // using the template in this path, relative
-struct SimpleTemplate {// should be used as a wrapper not sure how
-    messages: Vec<Messages> //Option with messages aka options(vec) or just a vec
+struct SimpleTemplate<'a> {
+    // should be used as a wrapper not sure how
+    messages: Vec<Messages>, //Option with messages aka options(vec) or just a vec
+    request_endpoint: &'a str,
+    profile_user: Option<User>,
+    user: Option<User>,
+    //g: Option<G>,
+    followed: bool, //Unsure how to define this properly
+
 }
 
-struct LayoutTemplateOld<'a> {// should be used as a wrapper not sure how
+struct LayoutTemplateOld<'a> {
+    // should be used as a wrapper not sure how
     title: &'a str,
     body: &'a str,
-    g: Option<G>,// Option is a nullable field user not defined
-    flashes: Option<Vec<Messages>> //Option with messages aka options(vec) or just a vec
+    g: Option<G>,                // Option is a nullable field user not defined
+    flashes: Option<Vec<Messages>>, //Option with messages aka options(vec) or just a vec
 }
 // String
-struct G {
-    db: Connection,
-    user: User,
-}
-//#[derive(Template)]
-//#[template(path = "../templates/timeline.html")] 
-struct TimelineTemplate<'a> {
-    name: String, // Is it not title 
-    messages:Vec<Messages>,// Vec<Message>, dynamic array of message structs 
-    user: Option<User>,
-    request_endpoint: &'a str,//just an URL does not need to be strict
-    profile_user: Option<User>,
-    followed: bool,//Unsure how to define this properly
-}
 
-#[derive(Debug)]
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>,
+//#[derive(Template)]
+//#[template(path = "../templates/timeline.html")]
+struct TimelineTemplate<'a> {
+    name: String,            // Is it not title
+    messages: Vec<Messages>, // Vec<Message>, dynamic array of message structs
+    user: Option<User>,
+    request_endpoint: &'a str, //just an URL does not need to be strict
+    profile_user: Option<User>,
+    followed: bool, //Unsure how to define this properly
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
     HttpServer::new(|| {
         App::new()
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[0; 64]))
+                .cookie_secure(false)
+                .build()
+            )
             .service(timeline)
             .service(fs::Files::new("/static", "../static").index_file("index.html"))
-            
     })
-    .bind(("0.0.0.0", 5000))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
@@ -75,11 +83,11 @@ fn get_database_string() -> String {
     "/tmp/mini-x.db".to_string()
 }
 
-fn connect_db() ->  Result<Connection>  {
+fn connect_db() -> Result<Connection> {
     Connection::open(get_database_string())
 }
 
-fn init_db()  -> rusqlite::Result<()>{
+fn init_db() -> rusqlite::Result<()> {
     let schema_sql = std::fs::read_to_string("schema.sql").unwrap();
     let conn = connect_db().unwrap();
 
@@ -87,21 +95,23 @@ fn init_db()  -> rusqlite::Result<()>{
     Ok(())
 }
 
-fn query_db(query: &str) {
-    
+fn query_db(query: &str) {}
+
+fn get_user_id(username: &str) {}
+
+fn g(session: Session) -> Result<()> {
+    let connection = connect_db()?;
+    Ok(())
 }
 
-fn get_user_id(username: &str ) {
-
-}
-
-fn g_mock() -> Result<G, rusqlite::Error> {
+fn g_mock() -> Result<G> {
     let connection = Connection::open_in_memory()?;
+
     Ok(G {
         db: connection,
         user: User {
-            user_id: 1,
-            user_name: String::from("Test Name"),
+            user_id: Uuid::new_v4(),
+            username: String::from("Test Name"),
         },
     })
 }
@@ -122,8 +132,19 @@ async fn timeline() -> impl Responder {
             pub_date: Utc::now(),
         },
     ];
-
-    return SimpleTemplate { messages };
+    
+    //if no user
+    //    Ok(HttpResponse::SeeOther()
+    // .header("Location", "/public")
+    // .finish());
+    let g_mock = g_mock().unwrap();
+    return SimpleTemplate { 
+        messages, 
+        request_endpoint:"/aa", 
+        profile_user: Some(User {user_id:Uuid::new_v4(), username:String::from("Name") }), 
+        user: Some(g_mock.user ), 
+        followed: false,
+    };
 }
 
 #[get("/public")]
@@ -146,7 +167,7 @@ async fn unfollow_user() -> impl Responder {
     return HelloTemplate { name: "AAAA" };
 }
 
-#[get("/add_message")]
+#[post("/add_message")]
 async fn add_message() -> impl Responder {
     return HelloTemplate { name: "AAAA" };
 }
@@ -164,60 +185,41 @@ async fn register() -> impl Responder {
 #[get("/logout")]
 async fn logout() -> impl Responder {
     return HelloTemplate { name: "AAAA" };
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
-async fn render_hello_template() -> impl Responder {
-    game();
-    return HelloTemplate { name: "AAAA" };
-}
+    // fn game() -> Result<()> {
+    //     let conn = Connection::open("/tmp/test.db")?;
 
-async fn cookie_test(session: Session) -> impl Responder {
-    if let Ok(Some(count)) = session.get::<i32>("counter") {
-        session.insert("counter", count + 1);
-    } else {
-        session.insert("counter", 0);
-    }
-    
-    let count = session.get::<i32>("counter").unwrap().unwrap();
-    HttpResponse::Ok().body(format!("Session has been refreshed {count} times"))
-}
+    //     conn.execute(
+    //         "CREATE TABLE person (
+    //               id              INTEGER PRIMARY KEY,
+    //               name            TEXT NOT NULL,
+    //               data            BLOB
+    //               )",
+    //         [],
+    //     )?;
+    //     let me = Person {
+    //         id: 0,
+    //         name: "Steven".to_string(),
+    //         data: None,
+    //     };
+    //     conn.execute(
+    //         "INSERT INTO person (name, data) VALUES (?1, ?2)",
+    //         params![me.name, me.data],
+    //     )?;
 
-fn game() -> Result<()> {
-    let conn = Connection::open("/tmp/test.db")?;
+    //     let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
+    //     let person_iter = stmt.query_map([], |row| {
+    //         Ok(Person {
+    //             id: row.get(0)?,
+    //             name: row.get(1)?,
+    //             data: row.get(2)?,
+    //         })
+    //     })?;
 
-    conn.execute(
-        "CREATE TABLE person (
-                  id              INTEGER PRIMARY KEY,
-                  name            TEXT NOT NULL,
-                  data            BLOB
-                  )",
-        [],
-    )?;
-    let me = Person {
-        id: 0,
-        name: "Steven".to_string(),
-        data: None,
-    };
-    conn.execute(
-        "INSERT INTO person (name, data) VALUES (?1, ?2)",
-        params![me.name, me.data],
-    )?;
+    //     for person in person_iter {
+    //         println!("Found person {:?}", person.unwrap());
+    //     }
 
-    let mut stmt = conn.prepare("SELECT id, name, data FROM person")?;
-    let person_iter = stmt.query_map([], |row| {
-        Ok(Person {
-            id: row.get(0)?,
-            name: row.get(1)?,
-            data: row.get(2)?,
-        })
-    })?;
-
-    for person in person_iter {
-        println!("Found person {:?}", person.unwrap());
-    }
-
-    Ok(())
-} 
+    //     Ok(())
+    // }
 }
