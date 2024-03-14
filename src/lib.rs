5dad357ce4e3b8c8108e4cfce0bdfad6e4af7a79
Marius Nielsen <mawn@itu.dk>
2024-03-14 19:39:06 +0100

    fix db string

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ pub fn establish_connection() -> SqliteConnection {
     dotenv().ok();
 
     //let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let database_url = "/tmp/mini-x.db";
+    let database_url = "/databases/mini-x.db";
 
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
