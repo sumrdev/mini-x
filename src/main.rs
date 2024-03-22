a8b31102b3a28c72a97da8d5b63bbacf2f3660b7
davidfeldner <60667332+davidfeldner@users.noreply.github.com>
2024-03-22 13:57:33 +0100

    postgres schema moved to docker compose dev (#48)
    Co-authored-by: Daniel <midf@itu.dk>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,8 @@
 use std::thread;
-
-use diesel::connection::SimpleConnection;
 use mini_x::api::api;
-use mini_x::establish_connection;
 use mini_x::frontend::frontend;
 
-fn init_db(){
-    const SCHEMA_SQL: &str = include_str!("schema.sql");
-    let mut conn = establish_connection();
-    let _ = conn.batch_execute(&SCHEMA_SQL);
-}
-
 fn main() {
-    let _ = init_db();
-
     let handle1 = thread::spawn(|| frontend::start());
     let handle2 = thread::spawn(|| api::start());
 
