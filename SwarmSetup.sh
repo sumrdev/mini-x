10aab03fe5079128dc4bed86cd9050c7fa4ed6f5
David Feldner <dafe@itu.dk>
2024-04-12 15:28:00 +0200

    mostly working swarmSetup and logging

--- SwarmSetup.sh
+++ SwarmSetup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 vagrant ssh droplet -c "docker swarm init --advertise-addr \$(hostname -I | cut -d\" \" -f1)"
-ip=$(vagrant ssh droplet -c 'hostname -I | cut -d" " -f1')
+allips=$(vagrant ssh droplet -c "hostname -I")
+ip=$(echo $allips | cut -d" " -f1)
+ip=$(echo $ip | grep -oE '\b([0-9]{1,3}\.){3}[0-9]{1,3}\b')
+ip=$ip:2377
+
 token=$(vagrant ssh droplet -c 'docker swarm join-token manager -q')
 
 echo "Token: $token"
 echo "IP: $ip"
 
-#vagrant ssh droplet2 -c "docker swarm join --token $token $ip:2377"
-#vagrant ssh droplet3 -c "docker swarm join --token $token $ip:2377"
+vagrant ssh worker1 -c "docker swarm join --token $token $ip"
+vagrant ssh worker2 -c "docker swarm join --token $token $ip"
